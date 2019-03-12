1f468d1bd5526f246a4b1725c363618b1061197f
KBSure <kbsure.dev@gmail.com>
2019-03-13 07:43:02 +0900

    프로젝트 생성 (Spring initializer 이용)

---
+++ src/main/java/com/project/shoppingmall2/Shoppingmall2Application.java
@@ -0,0 +1,13 @@
+package com.project.shoppingmall2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shoppingmall2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shoppingmall2Application.class, args);
+    }
+
+}
